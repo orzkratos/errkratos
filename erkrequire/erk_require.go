65d234b7a4efbdbc232ce6fc7136a60a8ff3855e
yangyile <example@gogit.com>
2024-08-30 14:32:21 +0700

    使用接口 require.TestingT 代替 *testing.T 指针

--- erkrequire/erk_require.go
+++ erkrequire/erk_require.go
@@ -1,17 +1,15 @@
 package erkrequire
 
 import (
-	"testing"
-
 	"github.com/go-kratos/kratos/v2/errors"
 	"github.com/stretchr/testify/require"
 )
 
-func No(t *testing.T, erk *errors.Error, msgAndArgs ...interface{}) {
+func No(t require.TestingT, erk *errors.Error, msgAndArgs ...interface{}) {
 	require.NoError(t, eke(erk), msgAndArgs...)
 }
 
-func Eo(t *testing.T, erk *errors.Error, msgAndArgs ...interface{}) {
+func Eo(t require.TestingT, erk *errors.Error, msgAndArgs ...interface{}) {
 	require.Error(t, eke(erk), msgAndArgs...) //这里必须传递个空才行，跟前面的情况相同
 }
 
@@ -22,7 +20,7 @@ func eke(erk *errors.Error) error {
 	return erk
 }
 
-func Is(t *testing.T, expected *errors.Error, actualEx *errors.Error, msgAndArgs ...interface{}) {
+func Is(t require.TestingT, expected *errors.Error, actualEx *errors.Error, msgAndArgs ...interface{}) {
 	require.Equal(t, bool(eke(expected) == nil), bool(eke(actualEx) == nil), msgAndArgs...)
 	if expected != nil && actualEx != nil {
 		require.Equal(t, expected.Reason, actualEx.Reason, msgAndArgs...)
--- go.mod
+++ go.mod
@@ -18,8 +18,8 @@ require (
 	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/sys v0.24.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240823204242-4ba0660f739c // indirect
-	google.golang.org/grpc v1.65.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240827150818-7e3bb234dfed // indirect
+	google.golang.org/grpc v1.66.0 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
