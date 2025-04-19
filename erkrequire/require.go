eea2f49a5e6f6c400107c2b4f2f97ff637fbd334
yangyile <gogit@github.com/go-xlan/gogit>
2025-04-19 16:14:29 +0700

    错误处理的封装确实不容易都第十七个版本了方案还没稳定

--- erkrequire/require.go
+++ erkrequire/require.go
@@ -7,27 +7,15 @@ import (
 )
 
 func NoError(t require.TestingT, erk *errors.Error, msgAndArgs ...interface{}) {
-	require.NoError(t, convert(erk), msgAndArgs...)
+	require.NoError(t, utils.ToError(erk), msgAndArgs...)
 }
 
 func Error(t require.TestingT, erk *errors.Error, msgAndArgs ...interface{}) {
-	require.Error(t, convert(erk), msgAndArgs...) //这里必须传递个空才行，跟前面的情况相同
-}
-
-func convert(erk *errors.Error) error {
-	if erk == nil {
-		return nil //这里必须做这样的转换，因为两个 nil 是不一样的
-	}
-	return erk
+	require.Error(t, utils.ToError(erk), msgAndArgs...) //这里必须传递个空才行，跟前面的情况相同
 }
 
 func Is(t require.TestingT, expected *errors.Error, erkParam *errors.Error, msgAndArgs ...interface{}) {
-	require.Equal(
-		t,
-		utils.GetValue(convert(expected) == nil),
-		utils.GetValue(convert(erkParam) == nil),
-		msgAndArgs...,
-	)
+	require.Equal(t, expected == nil, erkParam == nil, msgAndArgs...)
 	if expected != nil && erkParam != nil {
 		require.Equal(t, expected.Reason, erkParam.Reason, msgAndArgs...)
 		require.Equal(t, expected.Code, erkParam.Code, msgAndArgs...)
--- errors.go
+++ errors.go
@@ -2,7 +2,6 @@ package errkratos
 
 import (
 	"github.com/go-kratos/kratos/v2/errors"
-	"github.com/orzkratos/errkratos/internal/utils"
 )
 
 // Error 再定义个别名，使用它，能避免与官方的errors包名冲突
@@ -10,23 +9,6 @@ type Error = errors.Error
 
 // Erk 使用简单的名字简化代码
 type Erk = errors.Error
-
-// Ero 把 erk 包一层，但不实现 golang 的 error 接口，这样就能避免空指针也被当成是有 error 的，避免踩坑也避免时刻保持警惕，让开发者在写业务时保持放松状态
-type Ero struct {
-	Erk *Erk
-}
-
-// NewEro 返回的 Ero 只是把 erk 包一层
-func NewEro(erk *Erk) *Ero {
-	return &Ero{
-		Erk: erk,
-	}
-}
-
-// FmtEro 返回的 Ero 只是把 erk 包一层
-func FmtEro(newErk func(format string, args ...interface{}) *errors.Error, format string, args ...interface{}) *Ero {
-	return NewEro(newErk(format, args...))
-}
 
 // As 这里使用As就直接能指定类型，这样能够简便些，毕竟在这个语境下的目标类型确定
 func As(err error) (erk *errors.Error, ok bool) {
@@ -40,7 +22,7 @@ func As(err error) (erk *errors.Error, ok bool) {
 // Is 这里比较相等，直接使用确定的类型，假如要比较不确定的类型，就请用原始的就行
 func Is(erk *errors.Error, target *errors.Error) bool {
 	if erk == nil || target == nil {
-		return utils.GetValue(erk == nil && target == nil)
+		return erk == nil && target == nil
 	}
 	return erk.Is(target)
 }
